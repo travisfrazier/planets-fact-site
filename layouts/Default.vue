47bff4b4524f3416300613a725c63d0389b3dbd9
Travis Frazier <Travis.Frazier@ivanti.com>
2024-01-03 14:29:20 -0800

    updates

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,20 +1,34 @@
 <template>
 	<div class="container">
 		<div class="nav-wrapper">
+			<transition>
+				<nav class="mobile-nav" v-show="navOpen">
+					<NuxtLink to="/">Mercury</NuxtLink>
+					<NuxtLink to="venus">Venus</NuxtLink>
+					<NuxtLink to="earth">Earth</NuxtLink>
+					<NuxtLink to="mars">Mars</NuxtLink>
+					<NuxtLink to="jupiter">Jupiter</NuxtLink>
+					<NuxtLink to="saturn">Saturn</NuxtLink>
+					<NuxtLink to="uranus">Uranus</NuxtLink>
+					<NuxtLink to="neptune">Neptune</NuxtLink>
+				</nav>
+			</transition>
 			<h5>The Planets</h5>
-			<img :style= "[navOpen ? {opacity: .5} : {opacity: 1}]" @click="toggleNav" src="/images/icon-hamburger.svg" />
-            <transition>
-                <nav v-show="navOpen">
-                    <NuxtLink to="/">Mercury</NuxtLink>
-                    <NuxtLink to="venus">Venus</NuxtLink>
-                    <NuxtLink to="earth">Earth</NuxtLink>
-                    <NuxtLink to="mars">Mars</NuxtLink>
-                    <NuxtLink to="jupiter">Jupiter</NuxtLink>
-                    <NuxtLink to="saturn">Saturn</NuxtLink>
-                    <NuxtLink to="uranus">Uranus</NuxtLink>
-                    <NuxtLink to="neptune">Neptune</NuxtLink>
-                </nav>
-            </transition>
+			<img
+				:style="[navOpen ? { opacity: 0.5 } : { opacity: 1 }]"
+				@click="toggleNav"
+				src="/images/icon-hamburger.svg"
+			/>
+			<nav class="desktop-nav">
+				<NuxtLink to="/">Mercury</NuxtLink>
+				<NuxtLink to="venus">Venus</NuxtLink>
+				<NuxtLink to="earth">Earth</NuxtLink>
+				<NuxtLink to="mars">Mars</NuxtLink>
+				<NuxtLink to="jupiter">Jupiter</NuxtLink>
+				<NuxtLink to="saturn">Saturn</NuxtLink>
+				<NuxtLink to="uranus">Uranus</NuxtLink>
+				<NuxtLink to="neptune">Neptune</NuxtLink>
+			</nav>
 		</div>
 		<main>
 			<slot />
@@ -34,13 +48,11 @@ export default {
 		toggleNav() {
 			this.navOpen = !this.navOpen;
 		},
-        reshowNav() {
-            if (window.innerWidth > 630) {
-                this.navOpen = true;
-            } else {
-                this.navOpen = false;
-            }
-        }
+		reshowNav() {
+			if (window.innerWidth > 630) {
+				this.navOpen = false;
+			}
+		},
 	},
 	watch: {
 		$route() {
@@ -94,31 +106,42 @@ export default {
 		@media only screen and (max-width: 850px) {
 			margin-bottom: 10px;
 		}
+	}
+
+	.desktop-nav {
 		@media only screen and (max-width: 630px) {
-			margin-bottom: 0px;
-			position: absolute;
-			height: 100%;
-			background: black;
-			z-index: 9999;
-			width: 100%;
-			top: 71px;
+			display: none;
+		}
+	}
+
+	.mobile-nav {
+		display: none;
+		@media only screen and (max-width: 630px) {
 			display: flex;
-			flex-direction: column;
-			left: 0;
-			padding: 15px;
-			a {
-				font-size: 15px;
-			}
+		}
+		margin-bottom: 0px;
+		position: absolute;
+		height: 100%;
+		background: black;
+		z-index: 9999;
+		width: 100%;
+		top: 71px;
+		display: flex;
+		flex-direction: column;
+		left: 0;
+		padding: 15px;
+		a {
+			font-size: 15px;
 		}
 	}
 }
 .v-enter-active,
 .v-leave-active {
-  transition: opacity 0.5s ease;
+	transition: opacity 0.5s ease;
 }
 
 .v-enter-from,
 .v-leave-to {
-  opacity: 0;
+	opacity: 0;
 }
 </style>
