ba37960882919c872dddba1c2be9d3812702f1bd
Travis Frazier <Travis.Frazier@ivanti.com>
2024-01-03 13:36:33 -0800

    updates

--- components/Planet.vue
+++ components/Planet.vue
@@ -1,46 +1,55 @@
 <template>
-    <section>
-        <div class="planet-top">
-            <div class="planet-image">
-                <img v-show="overview || geology" :src="planet.images.planet" />
-                <img v-show="structure" :src="planet.images.internal" />
-                <img class="geology-img" v-show="geology" :src="planet.images.geology" />
-            </div>
-            <div class="planet-info">
-                <h1>{{ planet.name }}</h1>
-                <p v-if="overview">{{ planet.overview.content }}</p>
-                <p v-if="structure">{{ planet.structure.content }}</p>
-                <p v-if="geology">{{ planet.geology.content }}</p>
-                <div class="planet-source-wrapper">
-                    <span>Source : </span>
-                    <a :href="planet.overview.source">Wikipedia</a>
-                </div>
-                <div class="planet-buttons">
-                    <button v-bind:class="{ active: overview }" @click="viewOverview">01 Overview</button>
-                    <button v-bind:class="{ active: structure }" @click="viewStructure">02 Interal Structure</button>
-                    <button v-bind:class="{ active: geology }" @click="viewGeology">03 Surface Geology</button>
-                </div>
-            </div>
+    <main>
+        <div class="planet-buttons mobile">
+            <button v-bind:class="{ active: overview }" @click="viewOverview">Overview</button>
+            <button v-bind:class="{ active: structure }" @click="viewStructure">Structure</button>
+            <button v-bind:class="{ active: geology }" @click="viewGeology">Geology</button>
         </div>
-        <div class="planet-bottom">
-            <div class="planet-fact">
-                <h4>Rotation Time</h4>
-                <h2>{{ planet.rotation }}</h2>
+        <section>
+            <div class="planet-top">
+                <div class="planet-image">
+                    <img v-show="overview || geology" :src="planet.images.planet" />
+                    <img v-show="structure" :src="planet.images.internal" />
+                    <img class="geology-img" v-show="geology" :src="planet.images.geology" />
+                </div>
+                <div class="planet-info">
+                    <div>
+                        <h1>{{ planet.name }}</h1>
+                        <p v-if="overview">{{ planet.overview.content }}</p>
+                        <p v-if="structure">{{ planet.structure.content }}</p>
+                        <p v-if="geology">{{ planet.geology.content }}</p>
+                        <div class="planet-source-wrapper">
+                            <span>Source : </span>
+                            <a :href="planet.overview.source">Wikipedia</a>
+                        </div>
+                    </div>
+                    <div class="planet-buttons desktop">
+                        <button v-bind:class="{ active: overview }" @click="viewOverview">01 Overview</button>
+                        <button v-bind:class="{ active: structure }" @click="viewStructure">02 Interal Structure</button>
+                        <button v-bind:class="{ active: geology }" @click="viewGeology">03 Surface Geology</button>
+                    </div>
+                </div>
             </div>
-            <div class="planet-fact">
-                <h4>Revolution Time</h4>
-                <h2>{{ planet.revolution }}</h2>
+            <div class="planet-bottom">
+                <div class="planet-fact">
+                    <h4>Rotation Time</h4>
+                    <h2>{{ planet.rotation }}</h2>
+                </div>
+                <div class="planet-fact">
+                    <h4>Revolution Time</h4>
+                    <h2>{{ planet.revolution }}</h2>
+                </div>
+                <div class="planet-fact">
+                    <h4>Radius</h4>
+                    <h2>{{ planet.radius }}</h2>
+                </div>
+                <div class="planet-fact">
+                    <h4>Average Temp.</h4>
+                    <h2>{{ planet.temperature }}</h2>
+                </div>
             </div>
-            <div class="planet-fact">
-                <h4>Radius</h4>
-                <h2>{{ planet.radius }}</h2>
-            </div>
-            <div class="planet-fact">
-                <h4>Average Temp.</h4>
-                <h2>{{ planet.temperature }}</h2>
-            </div>
-        </div>
-    </section>
+        </section>
+    </main>
 </template>
 
 <script>
@@ -79,6 +88,9 @@
 <style lang="scss">
 section {
     padding: 126px 50px 56px;
+    @media only screen and (max-width: 630px) {
+        padding: 0px 25px 50px;
+    }
 }
 .planet-top {
     margin-bottom: 87px;
@@ -86,10 +98,32 @@ section {
     align-items: center;
     justify-content: space-between;
     gap: 50px;
+    @media only screen and (max-width: 850px) {
+        flex-direction: column;
+    }
+    @media only screen and (max-width: 630px) {
+        margin-bottom: 0px;
+        padding-top: 75px;
+    }
     .planet-info {
         max-width: 350px;
+        @media only screen and (max-width: 850px) {
+            max-width: 100%;
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            gap: 30px;
+            align-items: center;
+        }
+        @media only screen and (max-width: 630px) {
+            grid-template-columns: 1fr;
+        }
         p {
             margin-bottom: 24px;
+        }
+        div {
+            @media only screen and (max-width: 630px) {
+                text-align: center;
+            }
         }
     }
     .planet-image {
@@ -101,6 +135,9 @@ section {
             position: absolute;
             width: 175px;
             top: 70%;
+            @media only screen and (max-width: 630px) {
+                width: 75px;
+            }
         }
     }
 }
@@ -134,9 +171,12 @@ section {
     display: flex;
     flex-direction: column;
     gap: 16px;
+    @media only screen and (max-width: 630px) {
+        order: 0;
+    }
     button {
         cursor: pointer;
-        padding: 16px 28px 14px;
+        padding: 16px 0px 16px 28px;
         background: none;
         color: $color-white;
         border: 1px solid $color-white;
@@ -151,20 +191,63 @@ section {
             border-color: v-bind(color);
         }
     }
+    &.desktop {
+        @media only screen and (max-width: 630px) {
+            display: none;
+        }
+    }
+
+    &.mobile {
+        display: none;
+        width: 100%;
+        flex-direction: row;
+        @media only screen and (max-width: 630px) {
+            display: flex;
+        }
+        button {
+            border: transparent;
+            background: none;
+            width: 100%;
+            padding: 14px 0px;
+            text-align: center;
+            &.active {
+                border-bottom: 3px solid v-bind(color);
+            }
+        }
+    }
 }
 
 .planet-bottom {
     display: flex;
     justify-content: space-between;
     gap: 20px;
+    @media only screen and (max-width: 630px) {
+        flex-direction: column;
+        align-items: center;   
+        justify-content: center;
+    }
 }
 .planet-fact {
     width: 100%;
     border: 1px solid $color-white;
     padding: 12px;
+    @media only screen and (max-width: 630px) {
+        display: flex;
+        align-items: center;
+        justify-content: space-between; 
+        padding: 0px;
+    }
     h4 {
         opacity: .5;
         text-transform: uppercase;
+        @media only screen and (max-width: 630px) {
+            padding-left: 16px;
+        }
+    }
+    h2 {
+        @media only screen and (max-width: 630px) {
+            padding-right: 16px;
+        }
     }
 }
 </style>
--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,42 +1,124 @@
 <template>
-    <div class="container">
-        <div class="nav-wrapper">
-            <h5>The Planets</h5>
-            <nav>
-                <NuxtLink to="/">Mercury</NuxtLink>
-                <NuxtLink to="venus">Venus</NuxtLink>
-                <NuxtLink to="earth">Earth</NuxtLink>
-                <NuxtLink to="mars">Mars</NuxtLink>
-                <NuxtLink to="jupiter">Jupiter</NuxtLink>
-                <NuxtLink to="saturn">Saturn</NuxtLink>
-                <NuxtLink to="uranus">Uranus</NuxtLink>
-                <NuxtLink to="neptune">Neptune</NuxtLink>
-            </nav>
-        </div>
-        <main>
-            <slot />
-        </main>
-    </div>
+	<div class="container">
+		<div class="nav-wrapper">
+			<h5>The Planets</h5>
+			<img :style= "[navOpen ? {opacity: .5} : {opacity: 1}]" @click="toggleNav" src="/images/icon-hamburger.svg" />
+            <transition>
+                <nav v-show="navOpen">
+                    <NuxtLink to="/">Mercury</NuxtLink>
+                    <NuxtLink to="venus">Venus</NuxtLink>
+                    <NuxtLink to="earth">Earth</NuxtLink>
+                    <NuxtLink to="mars">Mars</NuxtLink>
+                    <NuxtLink to="jupiter">Jupiter</NuxtLink>
+                    <NuxtLink to="saturn">Saturn</NuxtLink>
+                    <NuxtLink to="uranus">Uranus</NuxtLink>
+                    <NuxtLink to="neptune">Neptune</NuxtLink>
+                </nav>
+            </transition>
+		</div>
+		<main>
+			<slot />
+		</main>
+	</div>
 </template>
 
 <script>
+export default {
+	data() {
+		return {
+			navOpen: false,
+			windowWidth: null,
+		};
+	},
+	methods: {
+		toggleNav() {
+			this.navOpen = !this.navOpen;
+		},
+        reshowNav() {
+            if (window.innerWidth > 630) {
+                this.navOpen = true;
+            } else {
+                this.navOpen = false;
+            }
+        }
+	},
+	watch: {
+		$route() {
+			this.navOpen = false;
+		},
+	},
+	mounted() {
+		window.addEventListener('resize', this.reshowNav);
+	},
+	destroyed() {
+		window.removeEventListener('resize', this.reshowNav);
+	},
+};
 </script>
 
 <style lang="scss">
 .nav-wrapper {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 14px 0px;
-    nav {
-        display: flex;
-        gap: 24px;
-        a {
-            transition: opacity .5s ease-in-out;
-            &:hover {
-                opacity: .5;
-            }
-        }
-    }
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 14px 0px;
+	border-bottom: 1px solid #ffffff25;
+	img {
+		display: none;
+	}
+	@media only screen and (max-width: 850px) {
+		flex-direction: column;
+		h5 {
+			margin-bottom: 30px;
+			@media only screen and (max-width: 630px) {
+				margin-bottom: 0px;
+			}
+		}
+		@media only screen and (max-width: 630px) {
+			flex-direction: row;
+			img {
+				display: block;
+				cursor: pointer;
+			}
+		}
+	}
+	nav {
+		display: flex;
+		gap: 24px;
+		a {
+			transition: opacity 0.5s ease-in-out;
+			&:hover {
+				opacity: 0.5;
+			}
+		}
+		@media only screen and (max-width: 850px) {
+			margin-bottom: 10px;
+		}
+		@media only screen and (max-width: 630px) {
+			margin-bottom: 0px;
+			position: absolute;
+			height: 100%;
+			background: black;
+			z-index: 9999;
+			width: 100%;
+			top: 71px;
+			display: flex;
+			flex-direction: column;
+			left: 0;
+			padding: 15px;
+			a {
+				font-size: 15px;
+			}
+		}
+	}
+}
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
